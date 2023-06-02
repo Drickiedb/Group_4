72f29c2b7c8e03e9aec18314f4fe2e9f96f9e640
Drickiedb <131675254+Drickiedb@users.noreply.github.com>
2023-06-02 14:42:39 +0200

    Setup of file, basic layout

---
+++ Computer-assisted drug discovery.py
@@ -0,0 +1,13 @@
+""""
+Group assignment group 4
+"""
+#pip install rdkit
+import numpy as np
+import pandas as pd
+import rdkit as rdk
+
+class DataPrep:
+    def __init__(self):
+        
+    def first_function(self):
+        #code
