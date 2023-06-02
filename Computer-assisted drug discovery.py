04e6ee71cf1b644f8e60f5a351f6f47ca7ce2789
Drickiedb <131675254+Drickiedb@users.noreply.github.com>
2023-06-02 14:57:19 +0200

    some testing done

--- Computer-assisted drug discovery.py
+++ Computer-assisted drug discovery.py
@@ -1,13 +1,16 @@
 """"
 Group assignment group 4
 """
-#pip install rdkit
+# pip install rdkit
 import numpy as np
 import pandas as pd
 import rdkit as rdk
 
+
 class DataPrep:
+
     def __init__(self):
-        
+        self.firstvariable = 0
+
     def first_function(self):
-        #code
+        self.firstvariable
