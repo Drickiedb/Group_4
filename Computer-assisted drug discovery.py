02a4cafa0a10dba016915c8eb09d3b98f9168ae7
Drickiedb <131675254+Drickiedb@users.noreply.github.com>
2023-06-05 21:54:20 +0200

    added relevant libraries

--- Computer-assisted drug discovery.py
+++ Computer-assisted drug discovery.py
@@ -4,8 +4,17 @@ Group assignment group 4
 # pip install rdkit
 import numpy as np
 import pandas as pd
-import rdkit as rdk
-
+import rdkit
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem import Draw
+from rdkit.Chem.Draw import IPythonConsole
+from rdkit.Chem import PandasTools
+smiles = 'COC(=O)c1c[nH]c2cc(OC(C)C)c(OC(C)C)cc2c1=O'
+mol = Chem.MolFromSmiles(smiles)
+print(mol)
+smi = Chem.MolToSmiles(mol)
+print(smi)
 
 class DataPrep:
 
